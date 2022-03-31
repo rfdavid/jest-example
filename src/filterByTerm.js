808840675807c25684c64240fbd74a2d23908279
Rui Felipe David <ruifelipe@hotmail.com>
2022-03-31 09:49:09 -0400

    Remove validation

--- src/filterByTerm.js
+++ src/filterByTerm.js
@@ -1,5 +1,5 @@
 function filterByTerm(inputArr, searchTerm) {
-  if (!searchTerm) throw Error("searchTerm cannot be empty");
+//  if (!searchTerm) throw Error("searchTerm cannot be empty");
   if (!inputArr.length) throw Error("inputArr cannot be empty"); // new line
   const regex = new RegExp(searchTerm, "i");
   return inputArr.filter(function(arrayElement) {
